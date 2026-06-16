bd751ba987c77738b261619509552f379406bfb5
SahilAglawe <sahilaglawe66@gmail.com>
2026-06-16 13:03:20 +0530

    first commit

---
+++ src/main/java/com/example/HMS_through_Springboot/HmsThroughSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.HMS_through_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsThroughSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsThroughSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HMS_through_Springboot/HmsThroughSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HMS_through_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsThroughSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
