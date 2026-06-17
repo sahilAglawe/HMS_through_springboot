1898ae66b4bc82a5d82b3c547337e5b2554609d3
SahilAglawe <sahilaglawe66@gmail.com>
2026-06-17 15:31:25 +0530

    RESTAPIS

---
+++ src/main/java/com/example/HMS_through_Springboot/dto/BloodGroupCountResponseEntity.java
@@ -0,0 +1,17 @@
+package com.example.HMS_through_Springboot.dto;
+
+import com.example.HMS_through_Springboot.entity.type.BloodGroupType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BloodGroupCountResponseEntity {
+
+    private BloodGroupType bloodGroupType;
+    private Long count;
+}
--- src/main/java/com/example/HMS_through_Springboot/repository/PatientRepository.java
+++ src/main/java/com/example/HMS_through_Springboot/repository/PatientRepository.java
@@ -1,11 +1,16 @@
 package com.example.HMS_through_Springboot.repository;
 
+import com.example.HMS_through_Springboot.dto.BloodGroupCountResponseEntity;
 import com.example.HMS_through_Springboot.entity.Patient;
 import com.example.HMS_through_Springboot.entity.type.BloodGroupType;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -22,5 +27,24 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
     @Query("SELECT p FROM Patient p where p.bloodGroup = ?1")
     List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
 
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
 
+    @Query("select new com.example.HMS_through_Springboot.dto.BloodGroupCountResponseEntity.java(p.bloodGroup," +
+            " Count(p)) from Patient p group by p.bloodGroup")
+//    List<Object[]> countEachBloodGroupType();
+    List<BloodGroupCountResponseEntity> countEachBloodGroupType();
+
+    @Query(value = "select * from patient", nativeQuery = true)
+    Page<Patient> findAllPatients(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
+
+
+    //    @Query("SELECT p FROM Patient p LEFT JOIN FETCH p.appointments a LEFT JOIN FETCH a.doctor")
+    @Query("SELECT p FROM Patient p LEFT JOIN FETCH p.appointments")
+    List<Patient> findAllPatientWithAppointment();
 }
