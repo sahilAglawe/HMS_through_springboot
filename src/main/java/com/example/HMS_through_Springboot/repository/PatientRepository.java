fac0929a265749d7ea108f1081d068cfe4a2f009
SahilAglawe <sahilaglawe66@gmail.com>
2026-06-17 14:34:29 +0530

    RESTAPIS

--- src/main/java/com/example/HMS_through_Springboot/repository/PatientRepository.java
+++ src/main/java/com/example/HMS_through_Springboot/repository/PatientRepository.java
@@ -1,8 +1,26 @@
 package com.example.HMS_through_Springboot.repository;
 
 import com.example.HMS_through_Springboot.entity.Patient;
+import com.example.HMS_through_Springboot.entity.type.BloodGroupType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Patient findByName(String name);
+
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+
+    List<Patient> findByBirthDateBetween(LocalDate startDate, LocalDate endDate);
+
+    List<Patient> findByNameContainingOrderByIdDesc(String query);
+
+    @Query("SELECT p FROM Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
+
 
 }
