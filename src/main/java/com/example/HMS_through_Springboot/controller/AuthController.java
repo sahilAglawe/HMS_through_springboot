000f2c1952e259845d1bd1ff5475162717aa8b30
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-06-25 15:47:41 +0530

    Add login and signup endpoints to AuthController

--- src/main/java/com/example/HMS_through_Springboot/controller/AuthController.java
+++ src/main/java/com/example/HMS_through_Springboot/controller/AuthController.java
@@ -1,4 +1,31 @@
 package com.example.HMS_through_Springboot.controller;
 
+import com.example.HMS_through_Springboot.dto.LoginRequestDto;
+import com.example.HMS_through_Springboot.dto.LoginResponseDto;
+import com.example.HMS_through_Springboot.dto.SignUpRequestDto;
+import com.example.HMS_through_Springboot.dto.SignupResponseDto;
+import com.example.HMS_through_Springboot.security.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.ok(authService.login(loginRequestDto));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody SignUpRequestDto signupRequestDto) {
+        return ResponseEntity.ok(authService.signup(signupRequestDto));
+    }
 }
