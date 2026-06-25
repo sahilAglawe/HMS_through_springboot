2edbb35bc9739a41be965c0fcda13b875a25ee62
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-06-25 15:55:57 +0530

    Add appointment creation and patient profile retrieval

--- src/main/java/com/example/HMS_through_Springboot/controller/PatientController.java
+++ src/main/java/com/example/HMS_through_Springboot/controller/PatientController.java
@@ -1,4 +1,33 @@
 package com.example.HMS_through_Springboot.controller;
 
+
+import com.example.HMS_through_Springboot.dto.AppointmentResponseDto;
+import com.example.HMS_through_Springboot.dto.CreateAppointmentRequestDto;
+import com.example.HMS_through_Springboot.dto.PatientResponseDto;
+import com.example.HMS_through_Springboot.service.AppointmentService;
+import com.example.HMS_through_Springboot.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
 public class PatientController {
+
+    private final PatientService patientService;
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/appointments")
+    public ResponseEntity<AppointmentResponseDto> createNewAppointment(@RequestBody CreateAppointmentRequestDto createAppointmentRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.createNewAppointment(createAppointmentRequestDto));
+    }
+
+    @GetMapping("/profile")
+    private ResponseEntity<PatientResponseDto> getPatientProfile() {
+        Long patientId = 4L;
+        return ResponseEntity.ok(patientService.getPatientById(patientId));
+    }
+
 }
