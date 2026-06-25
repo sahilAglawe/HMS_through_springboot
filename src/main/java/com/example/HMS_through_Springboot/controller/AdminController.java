83971eb7f613626e9405f634ca469bf11dfb8967
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-06-25 15:45:45 +0530

    Implement patient retrieval and doctor onboarding endpoints

--- src/main/java/com/example/HMS_through_Springboot/controller/AdminController.java
+++ src/main/java/com/example/HMS_through_Springboot/controller/AdminController.java
@@ -1,4 +1,33 @@
 package com.example.HMS_through_Springboot.controller;
 
+import com.example.HMS_through_Springboot.dto.DoctorResponseDto;
+import com.example.HMS_through_Springboot.dto.PatientResponseDto;
+import com.example.HMS_through_Springboot.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final PatientService patientService;
+    private final DoctorService doctorService;
+
+    @GetMapping("/patients")
+    public ResponseEntity<List<PatientResponseDto>> getAllPatients(
+            @RequestParam(value = "page", defaultValue = "0") Integer pageNumber,
+            @RequestParam(value = "size", defaultValue = "10") Integer pageSize
+    ) {
+        return ResponseEntity.ok(patientService.getAllPatients(pageNumber, pageSize));
+    }
+
+    @PostMapping("/onBoardNewDoctor")
+    public ResponseEntity<DoctorResponseDto> onBoardNewDoctor(@RequestBody OnboardDoctorRequestDto onboardDoctorRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.onBoardNewDoctor(onboardDoctorRequestDto));
+    }
 }
