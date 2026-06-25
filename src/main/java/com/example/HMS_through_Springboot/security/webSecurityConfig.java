b7de835912b6ddf202a0c0f80883cd4c450a93b4
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-06-25 15:16:57 +0530

    Import role types ADMIN and DOCTOR in webSecurityConfig

--- src/main/java/com/example/HMS_through_Springboot/security/webSecurityConfig.java
+++ src/main/java/com/example/HMS_through_Springboot/security/webSecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import static com.example.HMS_through_Springboot.entity.type.RoleType.ADMIN;
+import static com.example.HMS_through_Springboot.entity.type.RoleType.DOCTOR;
+
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
