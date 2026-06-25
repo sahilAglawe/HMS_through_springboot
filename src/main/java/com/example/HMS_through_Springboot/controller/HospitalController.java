05e29a2638585f005402da9d98476b6214e68a82
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-06-25 15:54:02 +0530

    Add endpoint to retrieve all doctors

--- src/main/java/com/example/HMS_through_Springboot/controller/HospitalController.java
+++ src/main/java/com/example/HMS_through_Springboot/controller/HospitalController.java
@@ -1,4 +1,23 @@
 package com.example.HMS_through_Springboot.controller;
 
+import com.example.HMS_through_Springboot.dto.DoctorResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public")
+@RequiredArgsConstructor
 public class HospitalController {
+
+    private final DoctorService doctorService;
+
+    @GetMapping("/doctors")
+    public ResponseEntity<List<DoctorResponseDto>> getAllDoctors() {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
+    }
 }
